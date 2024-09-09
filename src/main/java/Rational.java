de7f5fc69e544a60682067501a08e9a420af247d
therealmysticman <sarttra.pra@student.mahidol.ac.th>
2024-09-09 11:24:09 +0700

    update JUnit

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -1,43 +1,50 @@
-class Rational { 
-    long numerator,denominator; 
+class Rational {
+    long numerator,denominator;
 
-    class Illegal extends Exception { 
-        String reason; 
-        Illegal (String reason) { 
-            this.reason = reason; 
-        } 
-    } 
+    class Illegal extends Exception {
+        String reason;
+        Illegal (String reason) {
+            this.reason = reason;
+        }
+    }
 
     Rational() {
         // to be completed
+        this.numerator = 0;
+        this.denominator = 1;
     }
 
-    Rational(long numerator, long denominator) throws Illegal { 
+    Rational(long numerator, long denominator) throws Illegal {
         // to be completed
-    } 
+        this.numerator = numerator;
+        if(denominator == 0) throw new Illegal("Denominator==0");
+        this.denominator = denominator;
+    }
 
-    // find the reduce form 
-    private void simplestForm() { 
-        long computeGCD; 
-        computeGCD = GCD(Math.abs(numerator), denominator); 
-        numerator /= computeGCD; 
-        denominator /= computeGCD; 
-    } 
+    // find the reduce form
+    private void simplestForm() {
+        long computeGCD;
+        computeGCD = GCD(Math.abs(numerator), denominator);
+        numerator /= computeGCD;
+        denominator /= computeGCD;
+    }
 
-    // find the greatest common denominator 
-    private long GCD(long a, long b) { 
-        if (a%b ==0) return b; 
-        else return GCD(b,a%b); 
+    // find the greatest common denominator
+    private long GCD(long a, long b) {
+        if (a%b ==0) return b;
+        else return GCD(b,a%b);
     }
 
     /***
      * Compute an addition of the current rational number to another given rational number
      * @param x the rational number to be added to the current rational number
      */
-    public void add(Rational x) { 
+    public void add(Rational x) {
+        if(x.denominator == 0) return;
+
         numerator = (numerator * x.denominator) + (x.numerator * denominator);
-        denominator = (denominator * x.denominator); 
-        simplestForm(); 
+        denominator = (denominator * x.denominator);
+        simplestForm();
     }
 
     /***
@@ -46,14 +53,24 @@ class Rational {
      */
     public void subtract(Rational x) {
         // to be completed
+        if (x.denominator==0) return;
+
+        numerator = (numerator * x.denominator) - (x.numerator * denominator);
+        denominator = (denominator * x.denominator);
+        simplestForm();
     }
 
     /***
      * Compute a multiplication of the current rational number to another given rational number
      * @param x the rational number to be multiplied to the current rational number
      */
-    public void multiply(Rational x) { 
+    public void multiply(Rational x) {
         // to be completed
+        if (x.denominator == 0) return;
+
+        numerator = (numerator * x.numerator);
+        denominator = (denominator * x.denominator);
+        simplestForm();
     }
 
     /***
@@ -62,6 +79,11 @@ class Rational {
      */
     public void divide(Rational x) {
         // to be completed
+        if(x.denominator == 0) return; // Should be illegal even before we flip to multiply(?)
+
+        numerator = (numerator * x.denominator);
+        denominator = (denominator * x.numerator);
+        simplestForm();
     }
 
     /***
@@ -70,8 +92,15 @@ class Rational {
      * @return true if the given rational number equals to the current, false otherwise
      */
     public boolean equals(Object x) {
+        Rational n = (Rational) x;
+        n.simplestForm();
+        long x_nume = n.numerator;
+        long x_denu = n.denominator;
+        simplestForm();
+        return numerator == x_nume && denominator == x_denu;
+
         // to be completed
-        return true; // TODO: This needs to be modified.
+// TODO: This needs to be modified.
     }
 
     /***
@@ -82,16 +111,48 @@ class Rational {
      */
     public long compareTo(Object x) {
         // to be completed
-        return -1; // TODO: this needs to be modified.
+        Rational n = (Rational) x;
+        n.simplestForm();
+        simplestForm();
+        /*
+        We have two fractions a/b and c/d.
+        Let Y = (a/b - c/d)
+              = (ad - bc)/(bd)
+        Now,
+        if Y > 0 then a/b > c/d
+        if Y = 0 then a/b = c/d
+        if Y < o then a/b < c/d
+
+        Since bd is always positive, the sign of Y depends only on the
+        numerator (ad-bc). So we need to compute (ad-bc) only.
+         */
+        long a = numerator;
+        long b = denominator;
+        long c = n.numerator;
+        long d = n.denominator;
+        long ad = a*d;
+        long bc = b*c;
+        long y = ad - bc;
+        /*
+        if Y > 0 then a/b > c/d
+        if Y = 0 then a/b = c/d
+        if Y < o then a/b < c/d
+         */
+        if(y > 0) return 1;
+        else if(y == 0) return 0;
+        else
+            return -1;
+
     }
 
     /***
      * Give the formatted string of the rational number
      * @return the string representation of the rational number. For example, "1/2", "3/4".
      */
-    public String toString() { 
+    public String toString() {
         // to be completed
-        return ""; // TODO: This needs to be modified.
+        simplestForm();
+        return numerator + "/" + denominator; // TODO: This needs to be modified.
     }
 
     public static void main(String[] args) {
--- src/test/java/RationalTest.java
+++ src/test/java/RationalTest.java
@@ -13,4 +13,77 @@ public class RationalTest {
         x.add(y);
         Assert.assertEquals(3, x.numerator);
     }
+    @Test
+    public void testSubtract(){
+        Rational x = new Rational();
+        x.numerator = 3;
+        x.denominator = 2;
+
+        Rational y = new Rational();
+        y.numerator = 2;
+        y.denominator = 4;
+        x.subtract(y);
+        Assert.assertEquals(1, x.numerator);
+        Assert.assertEquals(1, x.denominator);
+    }
+
+
+    @Test
+    public void testMultiply(){
+        Rational x = new Rational();
+        x.numerator = 2;
+        x.denominator = 3;
+
+        Rational y = new Rational();
+        y.numerator = 9;
+        y.denominator = 2;
+        x.multiply(y);
+        Assert.assertEquals(3, x.numerator);
+        Assert.assertEquals(1, x.denominator);
+    }
+
+    @Test
+    public void testDivision(){
+        Rational x = new Rational();
+        x.numerator = 12;
+        x.denominator = 3;
+
+        Rational y = new Rational();
+        y.numerator = 2;
+        y.denominator = 1;
+        x.divide(y);
+        Assert.assertEquals(2, x.numerator);
+        Assert.assertEquals(1, x.denominator);
+    }
+
+    @Test
+    public void testEquals() throws Rational.Illegal {
+        Rational x = new Rational(2, 6);
+        Rational y = new Rational(4, 12);
+        Assert.assertEquals(x, y);
+        x = new Rational(1, 2);
+        y = new Rational(5, 9);
+        Assert.assertNotEquals(x, y);
+    }
+
+    @Test
+    public void testCompare() throws Rational.Illegal {
+        Rational x = new Rational(2, 4);
+        Rational y = new Rational(1, 2);
+        Assert.assertEquals(0, x.compareTo(y));
+        x = new Rational(3, 4);
+        y = new Rational(1, 2);
+        Assert.assertEquals(1, x.compareTo(y));
+        x = new Rational(1,3);
+        y = new Rational(1,2);
+        Assert.assertEquals(-1, x.compareTo(y));
+    }
+
+    @Test
+    public void testToString() throws Rational.Illegal {
+        Rational x = new Rational(3, 6);
+        String val = x.toString();
+        Assert.assertEquals("1/2", val);
+    }
+
 }
